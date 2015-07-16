5c5934fc7a9d6ce495dc99c898b744205dde0d1f
guor <abguorui0928@163.com>
2015-07-16 13:22:10 +0800

    去掉没用的依赖

--- src/main/java/org/fastdb/DBRow.java
+++ src/main/java/org/fastdb/DBRow.java
@@ -14,4 +14,12 @@ public class DBRow extends HashMap<String, Object> {
 	 * 
 	 */
 	private static final long serialVersionUID = 4247959745278175790L;
+
+	public String getString(String key) {
+		return get(key).toString();
+	}
+
+	public int getInt(String key) {
+		return Integer.parseInt(getString(key));
+	}
 }
