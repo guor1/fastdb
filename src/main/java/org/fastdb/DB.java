9cb00d0d7bb4b10717bc8cf42ee2ac6cfebfc4db
guor <abguorui0928@163.com>
2016-05-18 11:23:02 +0800

    修改连接池

--- src/main/java/org/fastdb/DB.java
+++ src/main/java/org/fastdb/DB.java
@@ -13,112 +13,108 @@ import javax.persistence.PersistenceException;
  */
 public class DB {
 
-	/**
-	 * Persist an entity.
-	 * <p>
-	 * If there is no current transaction one will be created and committed for
-	 * you automatically.
-	 * </p>
-	 * <p>
-	 * Save can cascade along relationships. For this to happen you need to
-	 * specify a cascade of CascadeType.ALL or CascadeType.PERSIST on the
-	 * OneToMany, OneToOne or ManyToMany annotation.
-	 * </p>
-	 * 
-	 * @param entity
-	 *            entity instance
-	 * @throws PersistenceException
-	 *             if occurs any SQLException
-	 */
-	public static void persist(Object entity) {
-		DBConfig.getPrimaryDBServer().persist(entity);
-	}
+    /**
+     * Persist an entity.
+     * <p>
+     * If there is no current transaction one will be created and committed for
+     * you automatically.
+     * </p>
+     * <p>
+     * Save can cascade along relationships. For this to happen you need to
+     * specify a cascade of CascadeType.ALL or CascadeType.PERSIST on the
+     * OneToMany, OneToOne or ManyToMany annotation.
+     * </p>
+     * 
+     * @param entity entity instance
+     * @throws PersistenceException if occurs any SQLException
+     */
+    public static void persist(Object entity) {
+        DBConfig.getPrimaryDBServer().persist(entity);
+    }
 
-	/**
-	 * Delete an entity.
-	 * 
-	 * @param entity
-	 *            entity instance
-	 * @throws PersistenceException
-	 *             if occurs any SQLException
-	 */
-	public static void delete(Object entity) {
-		DBConfig.getPrimaryDBServer().delete(entity);
-	}
+    public static <T> int delete(Class<T> beanType, Object primaryKey) {
+        return DBConfig.getPrimaryDBServer().delete(beanType, primaryKey);
+    }
 
-	public static <T> List<T> findList(Class<T> entityClass) {
-		return DBConfig.getPrimaryDBServer().findList(entityClass);
-	}
+    /**
+     * Delete an entity.
+     * 
+     * @param entity entity instance
+     * @throws PersistenceException if occurs any SQLException
+     */
+    public static void delete(Object entity) {
+        DBConfig.getPrimaryDBServer().delete(entity);
+    }
 
-	/**
-	 * Find entity by primary key. Search for an entity of the specified class
-	 * and primary key.
-	 * 
-	 * @param entityClass
-	 *            entity class
-	 * @param primaryKey
-	 *            primary key
-	 * @return the found entity instance or null if the entity does not exist
-	 * @throws PersistenceException
-	 *             if occurs any SQLException
-	 */
-	public static <T> T find(Class<T> entityClass, Object primaryKey) {
-		return DBConfig.getPrimaryDBServer().find(entityClass, primaryKey);
-	}
+    public static <T> List<T> findList(Class<T> entityClass) {
+        return DBConfig.getPrimaryDBServer().findList(entityClass);
+    }
 
-	/**
-	 * create a query based on a manual sql
-	 * 
-	 * @param sql
-	 *            sql to be executed
-	 * @return
-	 */
-	public static DBQuery createNativeQuery(String sqlString) {
-		return DBConfig.getPrimaryDBServer().createNativeQuery(sqlString);
-	}
+    /**
+     * Find entity by primary key. Search for an entity of the specified class
+     * and primary key.
+     * 
+     * @param entityClass entity class
+     * @param primaryKey primary key
+     * @return the found entity instance or null if the entity does not exist
+     * @throws PersistenceException if occurs any SQLException
+     */
+    public static <T> T find(Class<T> entityClass, Object primaryKey) {
+        return DBConfig.getPrimaryDBServer().find(entityClass, primaryKey);
+    }
 
-	/**
-	 * Start a new transaction putting it into a ThreadLocal.
-	 * 
-	 * @throws SQLException
-	 */
-	public static Transaction beginTransaction() throws SQLException {
-		return DBConfig.getPrimaryDBServer().beginTransaction();
-	}
+    /**
+     * create a query based on a manual sql
+     * 
+     * @param sql sql to be executed
+     * @return
+     */
+    public static DBQuery createNativeQuery(String sqlString) {
+        return DBConfig.getPrimaryDBServer().createNativeQuery(sqlString);
+    }
 
-	/**
-	 * Returns the current transaction or null if there is no current
-	 * transaction in scope.
-	 */
-	public static Transaction currentTransaction() {
-		return DBConfig.getPrimaryDBServer().currentTransaction();
-	}
+    /**
+     * Start a new transaction putting it into a ThreadLocal.
+     * 
+     * @throws SQLException
+     */
+    public static Transaction beginTransaction() throws SQLException {
+        return DBConfig.getPrimaryDBServer().beginTransaction();
+    }
 
-	/**
-	 * Commit the current transaction.
-	 */
-	public static void commitTransaction() {
-		DBConfig.getPrimaryDBServer().commitTransaction();
-	}
+    /**
+     * Returns the current transaction or null if there is no current
+     * transaction in scope.
+     */
+    public static Transaction currentTransaction() {
+        return DBConfig.getPrimaryDBServer().currentTransaction();
+    }
 
-	/**
-	 * Rollback the current transaction.
-	 */
-	public static void rollbackTransaction() {
-		DBConfig.getPrimaryDBServer().rollbackTransaction();
-	}
+    /**
+     * Commit the current transaction.
+     */
+    public static void commitTransaction() {
+        DBConfig.getPrimaryDBServer().commitTransaction();
+    }
 
-	/**
-	 * get DBServer by serverName
-	 * 
-	 * @param serverName
-	 * @return
-	 */
-	public static DBServer use(String serverName) {
-		return DBConfig.getDBServer(serverName);
-	}
+    /**
+     * Rollback the current transaction.
+     */
+    public static void rollbackTransaction() {
+        DBConfig.getPrimaryDBServer().rollbackTransaction();
+    }
 
-	public static DBServer usePrimaryDBServer() {
-		return DBConfig.getPrimaryDBServer();
-	}
+    /**
+     * get DBServer by serverName
+     * 
+     * @param serverName
+     * @return
+     */
+    public static DBServer use(String serverName) {
+        return DBConfig.getDBServer(serverName);
+    }
+
+    public static DBServer usePrimaryDBServer() {
+        return DBConfig.getPrimaryDBServer();
+    }
 }
--- src/test/java/fastdb/core/TestDBServer.java
+++ src/test/java/fastdb/core/TestDBServer.java
@@ -3,9 +3,9 @@ package fastdb.core;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.fastdb.DB;
 import org.fastdb.DBQuery;
 import org.fastdb.DBRow;
-import org.fastdb.DBServer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,66 +14,64 @@ import fastdb.core.bean.Contex;
 
 public class TestDBServer {
 
-	private DBServer dbServer;
+    @Before
+    public void init() {
+        DB.createNativeQuery("DROP TABLE IF EXISTS `wdyq_contex`").executeUpdate();
+        String sql = "CREATE TABLE `wdyq_contex` (`id` int(10) NOT NULL AUTO_INCREMENT, `host` varchar(100) NOT NULL, `doctype` varchar(10) NULL, `template` varchar(255) NOT NULL, `expression` varchar(255) NOT NULL, `lastoptime` timestamp NULL, PRIMARY KEY (`id`), UNIQUE KEY `template_uk` (`template`) USING BTREE) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8";
+        int d = DB.createNativeQuery(sql).executeUpdate();
+        Assert.assertEquals(0, d);
+    }
 
-	@Before
-	public void init() {
-		dbServer = new DBServer(H2Database.dataSource, "h2db");
-		dbServer.createNativeQuery("DROP TABLE IF EXISTS `wdyq_contex`").executeUpdate();
-		String sql = "CREATE TABLE `wdyq_contex` (`id` int(10) NOT NULL AUTO_INCREMENT, `host` varchar(100) NOT NULL, `doctype` varchar(10) NULL, `template` varchar(255) NOT NULL, `expression` varchar(255) NOT NULL, `lastoptime` timestamp NULL, PRIMARY KEY (`id`), UNIQUE KEY `template_uk` (`template`) USING BTREE) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8";
-		int d = dbServer.createNativeQuery(sql).executeUpdate();
-		Assert.assertEquals(0, d);
-	}
+    @Test
+    public void testNativeQuery() {
+        DBQuery dbQuery = DB.createNativeQuery("select * from wdyq_contex");
+        List<DBRow> resultList = dbQuery.findList();
+        Assert.assertEquals(0, resultList.size());
 
-	@Test
-	public void testNativeQuery() {
-		DBQuery dbQuery = dbServer.createNativeQuery("select * from wdyq_contex");
-		List<DBRow> resultList = dbQuery.findList();
-		Assert.assertEquals(0, resultList.size());
+        dbQuery = DB
+                .createNativeQuery("insert into `wdyq_contex` (`host`,`doctype`,`template`,`expression`,`lastoptime`) values (?,?,?,?,?)");
+        int p = 1;
+        dbQuery.setParameter(p++, "legal.firefox.news.cn");
+        dbQuery.setParameter(p++, "html");
+        dbQuery.setParameter(p++, "legal.firefox.news.cn/[d]/[d]/[d]/[*].html");
+        dbQuery.setParameter(p++, "c:article_content");
+        dbQuery.setParameter(p++, new Timestamp(System.currentTimeMillis()));
+        int d = dbQuery.executeUpdate();
+        Assert.assertEquals(1, d);
 
-		dbQuery = dbServer.createNativeQuery("insert into `wdyq_contex` (`host`,`doctype`,`template`,`expression`,`lastoptime`) values (?,?,?,?,?)");
-		int p = 1;
-		dbQuery.setParameter(p++, "legal.firefox.news.cn");
-		dbQuery.setParameter(p++, "html");
-		dbQuery.setParameter(p++, "legal.firefox.news.cn/[d]/[d]/[d]/[*].html");
-		dbQuery.setParameter(p++, "c:article_content");
-		dbQuery.setParameter(p++, new Timestamp(System.currentTimeMillis()));
-		int d = dbQuery.executeUpdate();
-		Assert.assertEquals(1, d);
+        dbQuery = DB.createNativeQuery("select * from `wdyq_contex`");
+        resultList = dbQuery.findList();
+        Assert.assertEquals(1, resultList.size());
+    }
 
-		dbQuery = dbServer.createNativeQuery("select * from `wdyq_contex`");
-		resultList = dbQuery.findList();
-		Assert.assertEquals(1, resultList.size());
-	}
+    @Test
+    public void testFind() {
+        Contex contex = DB.find(Contex.class, 34);
+        Assert.assertNotNull(contex);
+        Assert.assertEquals("legal.firefox.news.cn", contex.getHost());
+    }
 
-	@Test
-	public void testFind() {
-		Contex contex = dbServer.find(Contex.class, 34);
-		Assert.assertNotNull(contex);
-		Assert.assertEquals("legal.firefox.news.cn", contex.getHost());
-	}
+    @Test
+    public void testFindList() {
+        List<Contex> resultList = DB.findList(Contex.class);
+        Assert.assertEquals(2, resultList.size());
+    }
 
-	@Test
-	public void testFindList() {
-		List<Contex> resultList = dbServer.findList(Contex.class);
-		Assert.assertEquals(2, resultList.size());
-	}
+    @Test
+    public void testDelete() {
+        int d = DB.delete(Contex.class, 34);
+        Assert.assertEquals(1, d);
+    }
 
-	@Test
-	public void testDelete() {
-		int d = dbServer.delete(Contex.class, 34);
-		Assert.assertEquals(1, d);
-	}
-
-	@Test
-	public void testPersist() {
-		Contex contex = new Contex();
-		contex.setHost("legal.firefox.news.cn");
-		contex.setDoctype("html");
-		contex.setExpression("c:article_content");
-		contex.setTemplate("legal.firefox.news.cn/[d]/[d]/[d]/[*].jsp");
-		contex.setLastoptime(new Timestamp(System.currentTimeMillis()));
-		dbServer.persist(contex);
-		System.out.println(contex.getId());
-	}
+    @Test
+    public void testPersist() {
+        Contex contex = new Contex();
+        contex.setHost("legal.firefox.news.cn");
+        contex.setDoctype("html");
+        contex.setExpression("c:article_content");
+        contex.setTemplate("legal.firefox.news.cn/[d]/[d]/[d]/[*].jsp");
+        contex.setLastoptime(new Timestamp(System.currentTimeMillis()));
+        DB.persist(contex);
+        System.out.println(contex.getId());
+    }
 }
